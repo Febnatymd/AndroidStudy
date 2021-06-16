7d710421b6b4cac2bc310721f9e50cec46eb0738
Febnatymd <caidanan28ezb@126.com>
2021-06-16 16:27:54 +0800

    新建

--- UiStudy/app/src/main/java/hang/study/uistudy/MainActivity.java
+++ UiStudy/app/src/main/java/hang/study/uistudy/MainActivity.java
@@ -1,37 +1,16 @@
 package hang.study.uistudy;
 
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import java.lang.ref.WeakReference;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private long mStart = 0;
-    private long mTime = 0;
-    private TimeHandler mHandler;
-
-    private String logTag = "hangDebug";
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d(logTag,"onCreate");
-        mHandler = new TimeHandler(this);
-        //
-        TextView tvCounter = (TextView)findViewById(R.id.counter);
-        tvCounter.setText(DateUtils.formatElapsedTime(0));
     }
 
     @Override
@@ -54,74 +33,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onDestroy(){
-
-        super.onDestroy();
-    }
-
-    private void startTimer(){
-        mStart = System.currentTimeMillis();
-        mHandler.removeMessages(0);
-        mHandler.sendEmptyMessage(0);
-
-    }
-
-    private void stopTime(){
-        mHandler.removeMessages(0);
-    }
-
-    private boolean isTimerRunning(){
-        return mHandler.hasMessages(0);
-    }
-
-    private void resetTimer(){
-        //TODO resetTimer
-    }
-
-    @Override
-    public void onClick(View v) {
-        Log.d(logTag,"enter onClick "+v.getId());
-        if(v.getId() == R.id.start_stop){
-            //Log.d(logTag,"enter onClick ");
-            Button btn = (Button)findViewById(R.id.start_stop);
-            if(isTimerRunning()){
-                stopTime();
-                btn.setText(R.string.start);
-
-            }else{
-                startTimer();
-                btn.setText(R.string.stop);
-            }
-        }else if(v.getId() == R.id.reset){
-            //
-        }
-
-    }
-
-    private class TimeHandler extends Handler{
-        private WeakReference<MainActivity> mActivityRef;
-
-        public TimeHandler(MainActivity activity){
-            mActivityRef = new WeakReference<>(activity);
-        }
-
-        @Override
-        public void handleMessage(Message msg){
-            MainActivity activity = mActivityRef.get();
-            if(activity != null) {
-                long current = System.currentTimeMillis();
-                activity.mTime = current-activity.mStart;
-                activity.mStart = current;
-
-                TextView counter = (TextView)activity.findViewById(R.id.counter);
-                counter.setText(DateUtils.formatElapsedTime(activity.mTime/1000));
-
-                sendEmptyMessageDelayed(0, 250);
-            }
-        }
-
     }
 }
